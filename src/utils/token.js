cfcf9184ffd068c0eb27ad67aa81d608da730da0
NicholasZanetiAulivier <auliviernicholaszaneti@gmail.com>
2026-04-29 10:28:32 +0700

    Added changes to last PR

---
+++ src/utils/token.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const config = require('../core/config');
+
+async function generateUserJwt(user) {
+    return jwt.sign({
+        username: user.username || 'Tanpa Nama',
+        email: user.email,
+    }, config.secret.user, { expiresIn: '15m' })
+}
+
+async function refreshUserJwt(user) {
+    return jwt.sign({
+        username: user.username || 'Tanpa Nama',
+        email: user.email,
+    }, config.secret.user, { expiresIn: '7d' })
+}
+
+async function generateUserJwt(admin) {
+    return jwt.sign({
+        username: admin.username || 'Tanpa Nama',
+        email: admin.email,
+        super_admin: admin.super_admin,
+    }, config.secret.admin, { expiresIn: '15m' })
+}
+
+async function refreshUserJwt(user) {
+    return jwt.sign({
+        username: user.username || 'Tanpa Nama',
+        email: user.email,
+        super_admin: admin.super_admin,
+    }, config.secret.admin, { expiresIn: '7d' })
+}
