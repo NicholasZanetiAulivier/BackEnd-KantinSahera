2fae03af34a2522a32dbf05f400bdde4c8e2fa69
VincentWu503 <vincenthendry06@gmail.com>
2026-05-01 16:55:12 +0700

    Benerin sistem verifikasi by OTP & makai pino logger.\n Logging error SQL w panggil pino yg disediakan di template tahun lalu.\n Cara pemanggilan bisa tengok cara aku import modulenya.

--- src/components/endpoints/auth/index.js
+++ src/components/endpoints/auth/index.js
@@ -2,12 +2,10 @@ const express = require('express');
 
 const userAuthRouter = require('./user');
 const adminAuthRouter = require('./admin');
-const verificationRouter = require('./verify');
 
 const route = express.Router();
 
 route.use('/user', userAuthRouter);
 route.use('/admin', adminAuthRouter);
-route.use('/verify', verificationRouter);
 
 module.exports = route;
