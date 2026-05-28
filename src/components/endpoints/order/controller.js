cd7481c2e14342e01d52f9bc520b97792858f92b
NicholasZanetiAulivier <auliviernicholaszaneti@gmail.com>
2026-05-28 20:57:09 +0700

    Added used commits from nicoZA

--- src/components/endpoints/order/controller.js
+++ src/components/endpoints/order/controller.js
@@ -33,7 +33,7 @@ async function addCustomerCartItem(req, res, next) {
         const { menu_id, quantity } = value;
 
         if (await service.checkItemInCustomerCart(id, menu_id)) {
-            throw errorResponder(errors.DB_DUPLICATE_CONFLICT, "Sudah ada item ini di dalam cart!, gunakan PUT untuk mengupdate!");
+            throw errorResponder(errors.DB_DUPLICATE_CONFLICT, "Sudah ada item ini di dalam cart!, gunakan PATCH untuk mengupdate!");
         }
 
         await service.addCustomerCartItem(id, menu_id, quantity);
@@ -94,6 +94,9 @@ async function deleteCustomerCart(req, res, next) {
 async function getCartPrice(req, res, next) {
     try {
         const id = parseUserId(req.user.user_id);
+        const { building } = req.query;
+
+        const price = await service.getCartPrice(id, building == undefined ? false : true); //We assume for now a flat fee. Although this should depend on the building
 
         const price = await service.getCartPrice(id, true);
         return res.status(200).json({ price });
@@ -124,7 +127,7 @@ async function createOrder(req, res, next) {
         }
 
         const exists = await service.checkCustomerCartExists(id);
-        console.log(exists);
+        // console.log(exists);
         if (!exists) {
             throw errorResponder(errors.NOT_FOUND, "Tidak ada data cart untuk pengguna ini!");
         }
